3503a445b7fa6778e0e80939cd4ee2aefbbc55f3
Tim Dohring <tedohrin@edu.uwaterloo.ca>
2021-05-15 17:52:31 -0400

    Creating a 1.30 backup

---
+++ 1.30/2226968141/common/defines/00_innea.lua
@@ -0,0 +1,33 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NCountry.MAX_GOV_RANK = 6
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_2 = 50
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_3 = 75
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_X = 100
+NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_2 = 250
+NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_3 = 500
+NDefines.NCountry.ADDITIONAL_MIN_DEVELOPMENT_FOR_GOV_RANK_X = 1000
+NDefines.NCountry.CULTURAL_UNION_MIN_DEV = 2000
+NDefines.NCountry.CULTURAL_UNION_MIN_RANK = 5
+NDefines.NCountry.NAT_FOCUS_YEARS_RANK = 2
+NDefines.NCountry.CORE_LOSE = 100
+NDefines.NCountry.CORE_LOSE_CULTURE_GROUP = 200
+
+NDefines.NMilitary.FORT_PER_DEV_RATIO = 60 
+--50 is normal
+
+NDefines.NNationDesigner.MAX_GOVERNMENT_RANK = 6
+
+--AE_PROVINCE_CAP
+
+NDefines.NCountry.PS_IMPROVE_PROVINCE_BASE = 100
+NDefines.NCountry.SETTLEMENT_GROWTH_DEVELOPMENT_INCREASE = 1
+NDefines.NCountry.SETTLMENT_GROWTH_CHANCE_MULTIPLIER = 10
+NDefines.NCountry.SETTLMENT_GROWTH_CHANCE_MIN = 0.10
+NDefines.NCountry.SETTLEMENT_GROWTH_CHECK_INTERVAL = 365
+-- 1, 2.5, 0.05, 365 are base values of the above in order
+
+NDefines.NDiplomacy.VASSALIZE_BASE_DEVELOPMENT_CAP = 200
+NDefines.NDiplomacy.DEVELOPMENT_CAP_BASE = 20
